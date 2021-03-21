c175ea6441fa6ee08a09763a47d221e6989899ca
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-03-21 18:16:21 +0300

    Added reading config on startup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
+use std::{fs, process::exit};
+
+use config::{Config, Error as ConfigError};
+
 mod config;
 
 fn main() {
-    println!("Hello, world!");
+    // parse sc.toml
+    let config = {
+        let contents = fs::read_to_string("sc.toml");
+
+        let config = match contents {
+            Ok(contents) => contents,
+            Err(e) => {
+                eprintln!("Failed to parse config file with error: {}", e);
+                exit(-1);
+            }
+        }
+        .parse::<Config>();
+
+        match config {
+            Ok(config) => config,
+            Err(e) => match e {
+                ConfigError::TomlError(e) => {
+                    eprintln!("While parsing sc.toml an error occured: {}", e);
+                    exit(-1);
+                }
+                ConfigError::ConfigErrors(e) => {
+                    eprintln!("While parsing sc.toml errors occured:");
+
+                    for msg in e {
+                        eprintln!("{}", msg);
+                    }
+
+                    exit(-1);
+                }
+            },
+        }
+    };
+
+    // execute later code
+    println!("Config: {:#?}", config);
 }
