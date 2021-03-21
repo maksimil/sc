abbb446a8800e7a1cf83d813b6b88a1b63da9884
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-03-21 20:06:12 +0300

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
     process::exit,
 };
 
+use clap::clap_app;
 use config::{Config, Error as ConfigError};
 
 mod config;
@@ -11,56 +12,64 @@ mod config;
 const DEFAULT_SCRIPT_CONTENTS: &str = include_str!("sc_default.toml");
 
 fn main() {
-    // parse sc.toml
-    let config = {
-        let contents = fs::read_to_string("sc.toml");
+    // cli
+    let matches = clap_app!(sc =>
+        (version: env!("CARGO_PKG_VERSION"))
+        (author: env!("CARGO_PKG_AUTHORS"))
+        (about: "Manages script for your projects")
+        (@arg COMMAND: "Specifies which command you want to run")
+    )
+    .get_matches();
 
-        let config = match contents {
-            Ok(contents) => contents,
-            Err(e) => {
-                if e.kind() == ErrorKind::NotFound {
-                    eprintln!("Configuration file not found, create sc.toml for you? [Y/N]");
-                    let mut buf = String::new();
-                    io::stdin().read_line(&mut buf).unwrap();
-                    let buf = buf.trim();
-                    if buf == "Y" || buf == "y" {
-                        if let Err(e) = fs::write("sc.toml", DEFAULT_SCRIPT_CONTENTS) {
-                            eprintln!("While creating sc.toml an error occured: {}", e);
-                            exit(-1);
+    if let Some(command) = matches.value_of("COMMAND") {
+        // parse sc.toml
+        let config = {
+            let contents = fs::read_to_string("sc.toml");
+
+            let config = match contents {
+                Ok(contents) => contents,
+                Err(e) => {
+                    if e.kind() == ErrorKind::NotFound {
+                        eprintln!("Configuration file not found, create sc.toml for you? [Y/N]");
+                        let mut buf = String::new();
+                        io::stdin().read_line(&mut buf).unwrap();
+                        let buf = buf.trim();
+                        if buf == "Y" || buf == "y" {
+                            if let Err(e) = fs::write("sc.toml", DEFAULT_SCRIPT_CONTENTS) {
+                                eprintln!("While creating sc.toml an error occured: {}", e);
+                                exit(-1);
+                            } else {
+                                String::from(DEFAULT_SCRIPT_CONTENTS)
+                            }
                         } else {
-                            String::from(DEFAULT_SCRIPT_CONTENTS)
+                            exit(-1);
                         }
                     } else {
+                        eprintln!("Failed to parse config file with error: {}", e);
                         exit(-1);
                     }
-                } else {
-                    eprintln!("Failed to parse config file with error: {}", e);
-                    exit(-1);
                 }
             }
-        }
-        .parse::<Config>();
+            .parse::<Config>();
 
-        match config {
-            Ok(config) => config,
-            Err(e) => match e {
-                ConfigError::TomlError(e) => {
-                    eprintln!("While parsing sc.toml an error occured: {}", e);
-                    exit(-1);
-                }
-                ConfigError::ConfigErrors(e) => {
-                    eprintln!("While parsing sc.toml errors occured:");
-
-                    for msg in e {
-                        eprintln!("{}", msg);
+            match config {
+                Ok(config) => config,
+                Err(e) => match e {
+                    ConfigError::TomlError(e) => {
+                        eprintln!("While parsing sc.toml an error occured: {}", e);
+                        exit(-1);
                     }
+                    ConfigError::ConfigErrors(e) => {
+                        eprintln!("While parsing sc.toml errors occured:");
 
-                    exit(-1);
-                }
-            },
-        }
-    };
+                        for msg in e {
+                            eprintln!("{}", msg);
+                        }
 
-    // execute later code
-    println!("Config: {:#?}", config);
+                        exit(-1);
+                    }
+                },
+            }
+        };
+    }
 }
