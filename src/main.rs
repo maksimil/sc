3779feff41e646f87ee14d7961290601a83da9ab
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-03-21 18:51:03 +0300

    Added default config creation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::{fs, process::exit};
+use std::{
+    fs,
+    io::{self, ErrorKind},
+    process::exit,
+};
 
 use config::{Config, Error as ConfigError};
 
 mod config;
+
+const DEFAULT_SCRIPT_CONTENTS: &str = include_str!("sc_default.toml");
 
 fn main() {
     // parse sc.toml
@@ -12,8 +18,25 @@ fn main() {
         let config = match contents {
             Ok(contents) => contents,
             Err(e) => {
-                eprintln!("Failed to parse config file with error: {}", e);
-                exit(-1);
+                if e.kind() == ErrorKind::NotFound {
+                    eprintln!("Configuration file not found, create sc.toml for you? [Y/N]");
+                    let mut buf = String::new();
+                    io::stdin().read_line(&mut buf).unwrap();
+                    let buf = buf.trim();
+                    if buf == "Y" || buf == "y" {
+                        if let Err(e) = fs::write("sc.toml", DEFAULT_SCRIPT_CONTENTS) {
+                            eprintln!("While creating sc.toml an error occured: {}", e);
+                            exit(-1);
+                        } else {
+                            String::from(DEFAULT_SCRIPT_CONTENTS)
+                        }
+                    } else {
+                        exit(-1);
+                    }
+                } else {
+                    eprintln!("Failed to parse config file with error: {}", e);
+                    exit(-1);
+                }
             }
         }
         .parse::<Config>();
